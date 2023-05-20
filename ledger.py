4936c4cbc6893a65e5531f505f955aa21835ba4c
Averek7 <singhavanish2003@gmail.com>
2023-05-20 15:43:32 +0530

    ledger-done

--- ledger.py
+++ ledger.py
@@ -6,7 +6,6 @@ import json
 class BlockChain:
 
     def __init__(self):
-        
         # Genesis Block 
         self.chain = []
         self.create_block(owner = "creater", Reg_no="005", proof = 1, previous_hash = "0")
@@ -22,3 +21,21 @@ class BlockChain:
         }
         self.chain.append(block)
         return block
+    
+    def proof_work(self, previous_proof):
+        new_proof = 1
+        checkProof = False
+
+        while checkProof is False:
+            hashVal = hashlib.sha256(str(new_proof**2-previous_proof**2).encode()).hexdigest()
+
+            if hashVal[:4] == '0000':
+                checkProof = True
+            else:
+                new_proof += 1
+        
+        return new_proof
+    
+    def hash(self, block):
+        encodeBlock = json.dumps(block).encode()     # converts block(dict) to string
+        return hashlib.sha256(encodeBlock).hexdigest()
