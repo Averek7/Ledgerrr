b8cdebd0166d04e8347f43e4bfd3372c1053bdc4
Averek7 <singhavanish2003@gmail.com>
2023-05-20 15:22:48 +0530

    block-on-chain

---
+++ ledger.py
@@ -0,0 +1,24 @@
+import datetime 
+import hashlib
+import json
+from flask import Flask, jsonify, request
+
+class BlockChain:
+
+    def __init__(self):
+        
+        # Genesis Block 
+        self.chain = []
+        self.create_block(owner = "creater", Reg_no="005", proof = 1, previous_hash = "0")
+    
+    def create_block(self, owner, Reg_no, proof, previous_hash):
+        block = {
+            "owner": owner, 
+            'Reg_no': Reg_no, 
+            "index": len(self.chain) + 1,
+            "proof": proof,
+            "timestamp": str(datetime.datetime.now()),
+            "previous_hash": previous_hash
+        }
+        self.chain.append(block)
+        return block
